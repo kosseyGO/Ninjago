5c6f6ffedd142fa275e417b994eb52623736a78a
Kossey_MC <52999658+kosseyGO@users.noreply.github.com>
2023-07-27 11:14:58 +0100

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,10 +1,10 @@
 // Define the array of navigation links and their corresponding URLs
 const navigationLinks = [
-    { text: 'Home', url: '../index.html' },
-    { text: 'Watch', url: '../watch.html' },
-    { text: 'Games', url: '../games.html' },
-    { text: 'Info', url: '../info.html' },
-    { text: 'Contact', url: '../contact.php' },
+    { text: 'Home', url: 'index.html' },
+    { text: 'Watch', url: 'watch.html' },
+    { text: 'Games', url: 'games.html' },
+    { text: 'Info', url: 'info.html' },
+    { text: 'Contact', url: 'contact.php' },
     // Add more links as needed
 ];
 
@@ -14,7 +14,7 @@ function createNavigationBar() {
 
     // Create the logo image element
     const logoImage = document.createElement('img');
-    logoImage.src = '../logo.png';
+    logoImage.src = 'logo.png';
     logoImage.alt = 'My Logo';
     navElement.appendChild(logoImage);
 
